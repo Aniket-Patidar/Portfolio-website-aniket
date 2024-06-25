d88ece83995019ef07338984565b04c958dd7956
Aniket-Patidar <167737531+aniketpatidar123@users.noreply.github.com>
2024-06-25 16:06:58 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Aniket-Patidar",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1DoQU7svmHTqyx0BZ3MDlia5KvG-4BBib/view",
   linkedin: "https://www.linkedin.com/in/aniket-patidar-0b921b219/",
   insta: "https://www.instagram.com/_mr_aniket_patidar/",
   twitter: "",
